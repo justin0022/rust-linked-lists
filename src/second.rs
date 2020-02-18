1ed9c2e06be1d291a8eee7d4ad2085689c8c4072
Justin Lee <justinlee0022@gmail.com>
2020-02-18 14:43:26 -0800

    Made generic type

--- src/second.rs
+++ src/second.rs
@@ -1,73 +1,75 @@
-use std::mem;
-
-pub struct List {
-    head: Link,
+pub struct List<T> {
+  head: Link<T>,
 }
 
-// yay type aliases!
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+  elem: T,
+  next: Link<T>,
 }
 
-impl List {
-    pub fn new() -> Self {
-        List { head: None }
-    }
+impl<T> List<T> {
+  pub fn new() -> Self {
+      List { head: None }
+  }
 
-    pub fn push(&mut self, elem: i32) {
-        let new_node = Box::new(Node {
-            elem: elem,
-            next: self.head.take()
-        });
+  pub fn push(&mut self, elem: T) {
+      let new_node = Box::new(Node {
+          elem: elem,
+          next: self.head.take(),
+      });
 
-        self.head = Some(new_node);
-    }
+      self.head = Some(new_node);
+  }
 
-    pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, None) {
-            None => None,
-            Some(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
-    }
+  pub fn pop(&mut self) -> Option<T> {
+      self.head.take().map(|node| {
+          self.head = node.next;
+          node.elem
+      })
+  }
 }
 
-impl Drop for List {
-    fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, None);
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, None);
-        }
-    }
+impl<T> Drop for List<T> {
+  fn drop(&mut self) {
+      let mut cur_link = self.head.take();
+      while let Some(mut boxed_node) = cur_link {
+          cur_link = boxed_node.next.take();
+      }
+  }
 }
 
 #[cfg(test)]
 mod test {
   use super::List;
+
   #[test]
   fn basics() {
       let mut list = List::new();
 
+      // Check empty list behaves right
       assert_eq!(list.pop(), None);
 
+      // Populate list
       list.push(1);
       list.push(2);
       list.push(3);
 
+      // Check normal removal
       assert_eq!(list.pop(), Some(3));
       assert_eq!(list.pop(), Some(2));
-      assert_eq!(list.pop(), Some(1));
 
+      // Push some more just to make sure nothing's corrupted
       list.push(4);
       list.push(5);
 
+      // Check normal removal
       assert_eq!(list.pop(), Some(5));
       assert_eq!(list.pop(), Some(4));
+
+      // Check exhaustion
+      assert_eq!(list.pop(), Some(1));
       assert_eq!(list.pop(), None);
   }
 }
