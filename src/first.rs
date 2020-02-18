163bd37cdf43d7525af012ea519e4cbdd9494766
Justin Lee <justinlee0022@gmail.com>
2020-02-18 13:38:23 -0800

    Added test for List

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
   head: Link,
 }
@@ -15,5 +17,49 @@ struct Node {
 impl List {
   pub fn new() -> Self {
     List { head: Link::Empty }
+  }
+
+  pub fn push(&mut self, elem: i32) {
+    let new_node = Box::new(Node {
+      elem: elem,
+      next: mem::replace(&mut self.head, Link::Empty),
+    });
+
+    self.head = Link::More(new_node);
+  }
+
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
+  }
+}
+
+mod test {
+  use super::List;
+  #[test]
+  fn basics() {
+      let mut list = List::new();
+
+      assert_eq!(list.pop(), None);
+
+      list.push(1);
+      list.push(2);
+      list.push(3);
+
+      assert_eq!(list.pop(), Some(3));
+      assert_eq!(list.pop(), Some(2));
+      assert_eq!(list.pop(), Some(1));
+
+      list.push(4);
+      list.push(5);
+
+      assert_eq!(list.pop(), Some(5));
+      assert_eq!(list.pop(), Some(4));
+      assert_eq!(list.pop(), None);
   }
 }
